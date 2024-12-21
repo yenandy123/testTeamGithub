e383fe99df026220d8bbee4c1fff076cc7a6f455
A11110153 <146686755+A11110153@users.noreply.github.com>
2024-12-21 19:54:47 +0800

    Add files via upload

---
+++ idonnow/app/src/main/java/com/example/idonnow/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.idonnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.idonnow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
